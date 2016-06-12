73ff42d373dfecb4b4094edc4c2987d338b2ff6b
Camilo Parra <ccparram@unal.edu.co>
2016-06-12 00:59:25 -0500

    Implemented LoginInteractor

---
+++ app/src/main/java/com/example/ccparram/androidchat/login/LoginInteractorImpl.java
@@ -0,0 +1,21 @@
+package com.example.ccparram.androidchat.login;
+
+public class LoginInteractorImpl implements LoginInteractor{
+
+    private LoginRepository loginRepository;
+
+    @Override
+    public void checkSession() {
+        loginRepository.checkSession();
+    }
+
+    @Override
+    public void doSignUp(String email, String password) {
+        loginRepository.signUp(email, password);
+    }
+
+    @Override
+    public void doSignIn(String email, String password) {
+        loginRepository.signIn(email, password);
+    }
+}
